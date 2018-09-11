56d4c916964c61760c306e057be67a9913974d87
Edgar Pino <edgarbeltran@me.com>
2018-09-11 08:28:57 -0600

    Add tabs js

--- apps/clean_code_katas_web/assets/js/app.js
+++ apps/clean_code_katas_web/assets/js/app.js
@@ -19,3 +19,5 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import initTabs from './components/tabs'
+initTabs()
---
+++ apps/clean_code_katas_web/assets/js/components/tabs.js
@@ -0,0 +1,73 @@
+const TABS_SELECTOR = '.tabs > ul li'
+const ACTIVE_TAB_MENU_SELECTOR = '.tabs .is-active'
+const TAB_CONTENT_WRAPPER_SELECTOR = '.tabs-content-wrapper'
+const TAB_CONTENT_SELECTOR = `${TAB_CONTENT_WRAPPER_SELECTOR} .tab-content`
+const ACTIVE_TAB_CONTENT_SELECTOR = `${TAB_CONTENT_WRAPPER_SELECTOR} .tab-content.is-active`
+const ACTIVE_CLASS = 'is-active'
+
+function getTabWrapper(selector) {
+  return document.querySelector(selector)
+}
+
+function getTabs(selector) {
+  return document.querySelectorAll(selector)
+}
+
+function hasTabs(selector) {
+  return !!getTabWrapper(selector)
+}
+
+function getTargetTabContentElement(tabElement) {
+  const tabContentSelector = tabElement.dataset.tabContent
+  if(tabContentSelector) {
+    return document.querySelector(tabContentSelector)
+  } else {
+    console.error('no tab content selector for tab menu')
+  }
+}
+
+function isTargetTabContentOpen(tabElement) {
+  const tabContent = getTargetTabContentElement(tabElement)
+  return tabContent.classList.contains(ACTIVE_CLASS)
+}
+
+function getActiveTabContent(selector) {
+  return document.querySelector(selector)
+}
+
+function getActiveTabMenu(selector) {
+  return document.querySelector(selector)
+}
+
+function tabMenuClickHandler(event) {
+  event.preventDefault()
+  const currentTabMenu = event.currentTarget
+
+  if(isTargetTabContentOpen(currentTabMenu)) {
+    return 
+  }
+  
+
+  const activeTabContent = getActiveTabContent(ACTIVE_TAB_CONTENT_SELECTOR)
+  const activeTabMenu = getActiveTabMenu(ACTIVE_TAB_MENU_SELECTOR)
+  const currentTabContent = getTargetTabContentElement(currentTabMenu)
+  
+  activeTabContent.classList.remove(ACTIVE_CLASS)
+  activeTabMenu.classList.remove(ACTIVE_CLASS)
+  
+  currentTabContent.classList.add(ACTIVE_CLASS)
+  currentTabMenu.classList.add(ACTIVE_CLASS)  
+}
+
+function addClickEventHandler(elements) {
+  elements.forEach((element) => {
+    element.onclick = tabMenuClickHandler
+  })
+}
+
+export default function initTabs() {
+  if(!hasTabs(TAB_CONTENT_WRAPPER_SELECTOR)) return
+
+  const tabs = getTabs(TABS_SELECTOR)
+  addClickEventHandler(tabs)
+}
